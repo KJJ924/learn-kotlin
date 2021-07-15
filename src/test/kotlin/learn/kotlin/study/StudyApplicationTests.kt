53f3a046b23689ec76c17c0c857d7b91da796ce8
jaejoon <dkansk924@naver.com>
2021-07-15 16:17:54 +0900

    first commit

---
+++ src/test/kotlin/learn/kotlin/study/StudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package learn.kotlin.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
